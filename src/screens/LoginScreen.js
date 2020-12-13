2fdc5398126bcbc36fbcc6ee873df97089c68f90
Shreya <dhirshreya@gmail.com>
2020-12-13 23:25:51 +0530

    LoginUI update

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -9,23 +9,20 @@ const LoginScreen = ({navigation}) =>  {
     return (
       <View style={styles.container}>
         <View style={styles.circle}></View>
-        <View style={{ marginTop: 64 }}>
-          <Image
-            source={require("../../assets/chat.png")}
-            style={{width: 100, height: 100, alignSelf: "center"}}
-          />
+        <View style={{ marginTop: 84 }}>
         </View>
         <View style={{marginHorizontal: 32}}>
-          <Text style={styles.header}>Mobile Number</Text>
+          <Text style={styles.header}>Elixir</Text>
           <TextInput
             style={styles.input}
             placeholder="Enter Mobile Number"
+            placeholderTextColor="#a197a6"
+            keyboardType = 'number-pad'
           />
           <View style={{ alignItems: "flex-end", marginTop: 64 }}>
             <TouchableOpacity
               style={styles.continue}
-              onPress={() => navigation.navigate('ChatScreen')}
-            >
+              onPress={() => navigation.navigate('ChatScreen')}>
               <Ionicons name="arrow-forward-outline" size={24} color="#FFF" />
             </TouchableOpacity>
           </View>
@@ -39,38 +36,39 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F4F5F7',
+    backgroundColor: "#37476e",
   },
   circle: {
     width: 500,
     height: 500,
     borderRadius: 500 / 2,
-    backgroundColor: '#FFF',
+    backgroundColor: "#313f63",
     position: "absolute",
     left: -120,
     top: -20,
   },
   header: {
-    fontWeight: "900",
-    fontSize: 30,
-    color: "#514E5A",
+    fontWeight: "800",
+    fontSize: 50,
+    color: "#f5f7ed",
     marginTop: 32,
+    marginLeft: 110,
   },
   input: {
-    marginTop: 32,
+    marginTop: 100,
     height: 58,
     borderWidth: StyleSheet.hairlineWidth,
-    borderColor: "#BAB7C3",
+    borderColor: "#a197a6",
     borderRadius: 30,
     paddingHorizontal: 16,
-    color: "#514E5A",
+    color: "#6e669e",
     fontWeight: "600",
   },
   continue: {
     width: 70,
     height: 70,
     borderRadius: 70 / 2,
-    backgroundColor: "#9075E3",
+    backgroundColor: "#6e669e",
     alignItems: "center",
     justifyContent: "center",
   }
